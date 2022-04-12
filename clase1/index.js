255c5074d6e486d24466438fb1f103d182d65e4e
fililucas <lucas1929@live.com.ar>
2022-04-12 19:13:19 -0300

    master

---
+++ clase1/index.js
@@ -0,0 +1,91 @@
+/* console.log("hola"); */
+
+
+// Comentario simple
+
+/* Comentario de bloque */
+
+
+
+// Variables y valores, formas de declararlas
+
+/* let apellido = "garcia";
+
+console.log("1", apellido);
+apellido = "vela";
+console.log("2", apellido);
+
+
+const nombre= "andres"; */
+/* var numero = 3; */
+
+//typeof() me dice el tipo de datos de la variable
+/* let edad = prompt("ingrese su edad"); */
+/* alert(edad); */
+
+
+
+/* let nombre = prompt("Coloque su nombre por favor.");
+let apellido = prompt("Coloque su apellido por favor.");
+
+alert(nombre + " " + apellido) */
+
+
+//parseInt
+
+/* console.log(parseInt("4.6"));
+console.log(parseFloat("4.6")); */
+
+
+/* let dato = prompt("Colocar un dato")
+
+
+alert(dato) */
+
+
+/* 
+alert("En el siguiente cuadro podrá colocar un numero para una suma, numero con coma es valido.")
+let sumaUno = prompt("Colocar el primer numero para sumar");
+let sumaDos = prompt("Colocar el segundo numero para sumar");
+
+
+
+alert (parseFloat(sumaUno) + parseFloat(sumaDos));
+
+ */
+
+
+/* let usuario = prompt("Por favor ingrese usuario de su banco");
+let contrasena = prompt("Por favor ingrese la contraseña de su banco");
+let salida = "El usuario " + usuario + " de contraseña " + contrasena + " ha sido robada por el gato";
+alert(salida);
+ */
+
+
+
+
+//////////////////////////////////////////////////////// PRÁCTICA SUMA /////////////////////////////////////////////////////////////////
+
+
+
+/* alert("Bievenido a la calculadora virtual, a continuacion podra hacer una suma;");
+let primeraSuma = prompt("Por favor, introduzca el primer numero");
+let segundaSuma = prompt("Por favor, introduzca el segundo numero");
+let suma = parseFloat(primeraSuma) + parseFloat(segundaSuma);
+let resultado = "La suma de " + primeraSuma + " + " + segundaSuma + ", el resultado es: " + suma;
+alert(resultado); */
+
+
+
+
+
+//////////////////////////////////////////////////////// PRÁCTICA MULTIPLICACIÓN ///////////////////////////////////////////////////////
+
+
+
+alert("Bienvenido a la calculadora virtual, a continuacion podra hacer una multiplicacion;");
+let primerMultiplicacion = prompt("Por favor, introduzca el primer numero");
+let segundaMultiplicacion = prompt("Por favor, introduzca el segundo numero");
+let multiplicacion = parseFloat(primerMultiplicacion) * parseFloat(segundaMultiplicacion);
+let resultado = "La multiplicacion de " + primerMultiplicacion + " * " + segundaMultiplicacion + ", el resultado es: " + multiplicacion;
+alert(resultado);
---
+++ clase2/index.js
@@ -0,0 +1,12 @@
+/* 
+estructura del condicional if
+if(condicion){
+    operaciones a realizar
+} */
+
+let verdad = true;
+let falso = false;
+
+if(verdad){
+    console.log("verdadero");
+}
