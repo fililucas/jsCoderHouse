2064dd037baa13b9038fe412bb643fd0f6c45d3c
fililucas <lucas1929@live.com.ar>
2022-04-12 20:23:18 -0300

    if&else

--- clase2/index.js
+++ clase2/index.js
@@ -4,9 +4,71 @@ if(condicion){
     operaciones a realizar
 } */
 
-let verdad = true;
+/* let verdad = true;
 let falso = false;
 
 if(verdad){
     console.log("verdadero");
+} */
+
+
+/* if(3 == 3){
+    console.log("entramos")
 }
+*/
+
+/* <> */
+
+
+
+/* if(3 == 2){
+    console.log("es igual");
+}else{
+    console.log("no es igual");
+}
+
+console.log("final") */
+
+/* CONTROL DE FLUJO 
+let respuesta = prompt("Ingresar numero");
+
+if(respuesta == "5"){
+    alert("la entrada es 5");
+}else{
+    alert("la entrada no es igual a 5");
+}
+
+*/
+
+
+/* let precio = 100.5;
+
+if (precio < 20) {
+    console.log("El precio es menor que 20");
+}
+else if (precio < 50) {
+    console.log("El precio es menor que 50");
+}
+else if (precio < 100) {
+    console.log("El precio es menor que 100");
+}
+else {
+    console.log("El precio es mayor que 100");
+} */
+
+
+
+
+/* let valor = 4 > 5;
+console.log(valor);
+ */
+
+
+/* let nombre = "ana";
+
+if(nombre == "Ana"){
+    console.log("Es lo mismo");
+}else {
+    console.log("No es lo mismo");
+} */
+
