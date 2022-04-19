e07d3ceb1205a741b1a8c19be0293c86d98cef19
fililucas <lucas1929@live.com.ar>
2022-04-19 20:08:20 -0300

    arreglos

---
+++ clase4/index.js
@@ -0,0 +1,63 @@
+/* function saludar() {
+    console.log("Bienvenido");
+}
+
+saludar(); */
+
+
+
+/* function saludar(name) {
+    let saludo = `Bienvenido ${name}`;
+
+    alert(saludo);
+}
+
+
+let nombre = prompt("Ingresa tu nombre");
+
+saludar(nombre); */
+
+/* function solicitarNombre() {
+    let nombreIngresado = prompt("Ingresar nombre");
+    alert("El nombre ingresado es " + nombreIngresado);
+}
+
+
+solicitarNombre(); */
+
+
+
+/* function suma(x,y) {
+    return x + y;
+}
+
+let resultado = suma(1,2);
+
+console.log("El resultado es ", resultado); */
+
+
+
+function calculadora(num1, operacion, num2) {
+    switch (operacion) {
+        case "+":
+            return num1 + num2;
+
+        case "-":
+            return num1 - num2;
+
+        case "*":
+            return num1 * num2;
+
+        case "/":
+            return num1 / num2;
+
+        default:
+            return "Operacion no definida.";
+    }
+}
+
+
+
+let resultado = calculadora(10,"/", 2);
+
+console.log(resultado);
